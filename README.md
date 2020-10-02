e58e2ccb8d7eff2327350806171afb02def50a5b
dunntooni <dun15014@byui.edu>
2020-10-01 18:33:21 -0600

    Added ability to write to file and documentation (including comments)

---
+++ README.md
@@ -0,0 +1,24 @@
+# Overview
+
+
+This is a file-reading program that serves as a sort of prototype of a database accessor. It can read any file in the right format. Included are three files, named class1.txt, class2.txt, and class3.txt. The program displays the information in the file and allows the user to add to it. If the user adds to the file, it re-displays with the updated information.
+
+# Development Environment
+
+* Python 3.8.5
+* Visual Studio Code
+
+# Execution
+
+To run: `python main.py`   
+<b>Class 1 without adding data:</b>
+![Class 1 with no data added](class1NoDataAdded.png)
+<b>Class 2 with a new student being added:</b>
+![Class 2 with added data](class2DataAdded.png)
+
+# Useful Websites
+
+
+1. [Python Reference](https://docs.python.org/3/genindex-I.html)
+2. [Stack Overflow Python Questions](https://stackoverflow.com/questions/tagged/python)
+3. [W3Schools Python Tutorial](https://www.w3schools.com/python/)
--- main.py
+++ main.py
@@ -3,20 +3,30 @@ This program will read a list from a file of students
 and display the class correctly.
 Provides a name, grade, age in years,
 and whether or not they're graduating this year.
+Also provides the option to add a new student
+to the roster.
 """
 
+# Gets a file name from the user. There's no input handling
+# in this version, so be careful. If the file name isn't valid,
+# the program will not work.
 def promptFileName():
     fileName = input("Please enter the file name: ")
     return fileName
 
-def readfile(fileName):
+# Reads the file line by line and stores it in a list of strings
+def readFile(fileName):
     print("Reading File...")
     f = open(fileName, "rt")
     classList = []
     classList = f.readlines()
-
+    f.close()
     return classList
 
+# Displays the information contained in the files. Converts
+# their age to a birth year just for fun. I get that it
+# could be inaccurate depending on their birthday, but
+# I don't care too much.
 def displayClassList(classList):
     print(" ")
     print("Class Info:\n")
@@ -27,10 +37,38 @@ def displayClassList(classList):
         #print('{:15}'.format('test') + '{:8}'.format('test') + "test")
         studentInfo = x.split(" ")
         print('{:15}'.format(studentInfo[0]) + '{:8}'.format(studentInfo[1])\
-             + '{:13}'.format(2020 - int(studentInfo[2])) + studentInfo[3], end="")
+             + '{:13}'.format(str(2020 - int(studentInfo[2]))) + studentInfo[3], end="")
     print("\n")
+
+# Prompts the user for all the information about the student
+# that it needs. Be careful not to input invalid information.
+# I didn't do any input checking on this one either.
+def writeFile(fileName):
+    f = open(fileName, "a")
+    name = input("Please input the student's name: ")
+    grade = input("Please input the student's current grade out of 100: ")
+    bYear = input("Please input the student's birth year: ")
+    graduating = input("Is the student set to graduate this year? (Y/N) ")
+    if graduating == "Y" or graduating == "y" or graduating == "yes"\
+        or graduating == "YES" or graduating == "Yes":
+        graduating = "Yes"
+    elif graduating == "N" or graduating == "n" or graduating == "no"\
+        or graduating == "NO" or graduating == "No":
+        graduating = "No"
+    else:
+        graduating = "Unknown"
+    f.write("\n" + name + " " + grade + " " + str(2020 - int(bYear)) + " " + graduating)
+    f.close()
+
 
 fileName = promptFileName()
 classList = []
-classList = readfile(str(fileName))
+classList = readFile(str(fileName))
 displayClassList(classList)
+writeFilePrompt = input("Would you like to add a student to this roster? (Y/N)")
+if writeFilePrompt == "y" or writeFilePrompt == "Y" or writeFilePrompt == "yes"\
+    or writeFilePrompt == "Yes" or writeFilePrompt == "YES":
+    writeFile(fileName)
+    classList = readFile(str(fileName))
+    print("\nUpdating class list...")
+    displayClassList(classList)
